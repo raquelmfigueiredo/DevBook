634f04f0a2db038c39c89b64e8ccf33db78f0b60
raquelmfigueiredo <raquelhmfigueiredo@gmail.com>
2024-06-25 07:45:59 -0300

    Creating connection to the database

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 // go get github.com/gorilla/mux -> para conseguir utilizar as rotas.
 import (
-	"api/API/config"
+	"api/API/src/config"
 	"api/API/src/router"
 	"fmt"
 	"log"
@@ -11,9 +11,8 @@ import (
 
 func main(){
 	config.Carregar()
-
-	fmt.Println("Rodando API!!")
 	r := router.Gerar()
 
+	fmt.Printf("Escutando na porta %d", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
